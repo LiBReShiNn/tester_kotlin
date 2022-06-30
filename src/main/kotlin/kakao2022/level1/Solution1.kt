8b4481cf10db64c368ed2bc6117d4c0f8c9d635e
HyejiShin <h.j.shin@plendar.com>
2022-06-30 16:12:49 +0900

    Initial commit

---
+++ src/main/kotlin/kakao2022/level1/Solution1.kt
@@ -0,0 +1,14 @@
+package kakao2022.level1
+
+class Solution1 {
+    fun solution(id_list: Array<String>, report: Array<String>, k: Int): IntArray =
+        report.map { it.split(" ") }.groupBy { it[1] }
+            .asSequence()
+            .map { it.value.distinct() }
+            .filter { it.size >= k }
+            .flatten()
+            .map { it[0] }
+            .groupingBy { it }
+            .eachCount()
+            .run { id_list.map { getOrDefault(it, 0) }.toIntArray() }
+}
